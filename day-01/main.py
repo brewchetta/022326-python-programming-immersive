3bdcf288beac6cc34a66f863eee8b5548fcf662b
brewchetta <chett.tiller@gmail.com>
2026-02-23 12:05:22 -0500

    120 lines of python goodness

--- day-01/main.py
+++ day-01/main.py
@@ -2,3 +2,119 @@
 # python day-01/main.py
 # OR
 # python3 day-01/main.py
+
+# comment
+
+# DOES A COMMENT DO ANYTHING???
+# Not really... unless we want to write for ourselves and others
+
+# VARIABLES
+
+# store values
+
+# declare a variable
+my_variable = "Hello I am a variable"
+# name      equal   value
+
+# TERMINAL - COMMAND LINE
+
+print( my_variable )
+
+my_variable = "something else"
+
+print( my_variable )
+
+my_variable = "I have changed again"
+
+print(my_variable)
+
+# PYTHON IS LINEAR
+
+my_second_var = "I am the second variable"
+my_third_var = "I am the third variable"
+
+print( my_second_var )
+
+# DATA TYPES #
+
+# STRING --> text
+
+"I am a string"
+'I am also a string'
+"              " # <<< also a string
+"123456789" # <<< also a string
+"!@#$%^&*()" # <<< also a string
+
+my_string = "Hello my name is \"Chett\""
+
+# type will show you the data type
+what_type = type( my_string )
+
+print( what_type )
+
+# concatenation a.k.a. adding strings together
+brand_new_string = my_string + " and I like being a string"
+
+# formatted string / f string
+name = "Chett"
+age = "21"
+hobbies = "running around in the snow"
+
+f"{ name } is {age} and likes {hobbies}"
+
+# NUMBERS
+
+# integer
+type(21)
+type(-21)
+type(1000000000000000000000000)
+
+# float
+type( 2.1 )
+type( 1.0 )
+
+# math operations
+
+1 + 1 # addition
+2 - 1 # subtraction
+3 * 3 # multiply
+4 / 5 # divide
+6 % 3 # modulo
+
+# modulo lets you know what the remainder is of division between two numbers
+15 % 3 # 0 remainder
+16 % 3 # 1 remainder
+7 % 3 # 1 remainder
+
+# string-o-tizing data (stringify or string conversion)
+str(15) # "15"
+
+# BOOLEAN ###
+
+True
+False
+
+# FUNCTION ##############
+
+# D.R.Y. dont repeat yourself
+
+result_1 = 12 % 3 == 0
+print(result_1)
+result_2 = 55 % 3 == 0
+print(result_2)
+result_3 = 5678 % 3 == 0
+print(result_3)
+result_4 = 92 % 3 == 0
+print(result_4)
+result_5 = 90 % 3 == 0
+print(result_5)
+result_6 = 9000 % 3 == 0
+print(result_6)
+result_7 = 13 % 3 == 0
+print(result_7)
+
+def is_divisible_by_three(number):
+    result = number % 3 == 0
+    print(result)
+
+is_divisible_by_three(12)
