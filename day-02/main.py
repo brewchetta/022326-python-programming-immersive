5f681f2a3664d302f0195208b26da0ed63bbae47
Chett Tiller <chett.tiller@gmail.com>
2026-02-24 11:33:16 -0500

    scope and exercises

--- day-02/main.py
+++ day-02/main.py
@@ -1,5 +1,57 @@
 # SCOPE
 
+my_global_var = "I am a global variable"
+
+def i_have_local_stuff():
+    # START OF FUNCTION
+    print(my_global_var)
+    # the local variable is locked inside the function
+    local_var = "I am local"
+    print(local_var)
+    # END OF FUNCTION
+
+# this is fine though
+print(my_global_var)
+
+# this will error out because the local variable is not in scope
+# print(local_var)
+
+
+we_all_do_this = "breathe"
+
+def france():
+    # this doesnt overwrite the global it makes a LOCAL variable
+    we_all_do_this = "oui oui baguette"
+    print(we_all_do_this)
+
+def france_two():
+    # this WILL overwrite the global
+    global we_all_do_this
+    we_all_do_this = "oui oui baguette"
+    print(we_all_do_this)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # LISTS
--- day-02/review.py
+++ day-02/review.py
@@ -1,12 +1,27 @@
 # Monday Review Practice #
 
+# to run this in the terminal:
+
+# python -i day-02/review.py
+
+# the -i means it will run / load the file but also open a REPL at the end so we can continue interacting with variables and functions
 
 # Define a new function create_full_name() which takes in two arguments: first_name and last_name as strings
 # 	the function returns the first_name and last_name as a single string
 # 	Example:	create_full_name("Bob", "Marley") >>> "Bob Marley"
 
-def create_full_name():
-    pass
+
+def create_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+
+# with inputs
+# def create_full_name():
+#     first_name = input("Enter first name: ")
+#     last_name = input("Enter last name: ")
+
+#     return f"{first_name} {last_name}"
+#     # return first_name + " " + last_name
 
 
 # Define a new function c_to_f() which takes in one argument: temp_celsius as a number
@@ -14,5 +29,5 @@ def create_full_name():
 # 	the formula for conversion is F = (C * 9/5) + 32
 # 	Example:	c_to_f(30) >>> 86.0
 
-def c_to_f():
-	pass
+def c_to_f(temp_celsius):
+    return (temp_celsius * 9/5) + 32
